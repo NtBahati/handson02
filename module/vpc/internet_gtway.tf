d3c9e286e3c90dd9434de84635aca6d7093d5f69
devopseasylearning <info@devopseasylearning.com>
2025-03-18 20:52:40 -0400

    in progress

--- module/vpc/internet_gtway.tf
+++ module/vpc/internet_gtway.tf
@@ -2,6 +2,6 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = format("internet-gateway-%s", var.tags["environment"])
+    Name = format("internet-gateway-%s-%s-%s", var.tags["project"], var.tags["created_by"], var.tags["environment"])
   }
 }
